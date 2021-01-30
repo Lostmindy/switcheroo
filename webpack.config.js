2a9ad32f8c83f22cbf0d05d74fffb8dbe9ab634a
Cædrik <caedrikbl@gmail.com>
2021-01-30 00:04:50 -0500

    webpack config library

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,6 @@ const path = require('path');
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		library: 'Monomer',
-	    libraryTarget: 'var',
-	    libraryExport: 'default',
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist')
 	},
