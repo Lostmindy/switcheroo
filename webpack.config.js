b71319a7daa7e63b38fab9576786744301cf750b
Cædrik <caedrikbl@gmail.com>
2021-01-29 23:15:12 -0500

    update workflow of plugins + build

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,10 @@ module.exports = {
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist')
-	}
+	},
+	resolve: {
+    	alias: {
+      		core: path.resolve(__dirname, "../../core")
+    	}
+  	}
 };
